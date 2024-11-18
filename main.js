cc8c3ac341daa19b81e91091aa292a52c5300595
shahmaran0207 <shahmaran0207@naver.com>
2024-11-18 15:41:24 +0900

    get exchange_rate

---
+++ main.js
@@ -0,0 +1,20 @@
+let currencyRaio={
+    USD:{
+        KRW: 1394.98,
+        USD: 1,
+        VND: 25385.00,
+        unit: "달러"
+    },
+    KRW:{
+        KRW: 1,
+        USD: 0.00072,
+        vnd: 18.20,
+        unit: "원"
+    },
+    VND:{
+      USD: 0.000039,
+      VND: 1,
+      KRW: 0.055,
+      unit: "동"
+    }
+};
